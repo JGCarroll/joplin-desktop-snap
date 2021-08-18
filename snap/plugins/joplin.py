89f7545f0e8bc3c0a7ac09cc4206c76fdf1b98e5
James Carroll <eloquism@gmail.com>
2021-08-18 16:31:37 +0100

    Fetch NPM via snap
    Should benefit building on alternative CPU architectures.
    Also mild security benefits with more package verification being done.
    And less manual effort of bumping the latest patches up.
    
    I've no idea why `npm run dist` doesn't work, but running the same executables
    as `npm run dist` would run gets the job done all the same.

--- snap/plugins/joplin.py
+++ snap/plugins/joplin.py
@@ -10,19 +10,16 @@ class PluginImpl(PluginV2):
 			"$schema": "http://json-schema.org/draft-04/schema#",
 			"type": "object",
 			"additionalProperties": False,
-			"properties": {"node-version": {"type": "string"}},
-			"required": ["node-version"]
 		}
 
 	def get_build_snaps(self) -> Set[str]:
-		return set()
+		return {"node/16/stable"}
 
 	def get_build_packages(self) -> Set[str]:
 		return {"python", "rsync", "libsecret-1-dev", "curl"}
 
 	def get_build_environment(self) -> Dict[str, str]:
 		return dict(
-			PATH="${SNAPCRAFT_PART_BUILD}/NodeJS/bin:${PATH}",
 			npm_config_unsafe_perm="true",
 			SUDO_UID="0",
 			SUDO_GID="0",
@@ -30,15 +27,6 @@ class PluginImpl(PluginV2):
 			npm_config_prefer_offline="true"
 		)
 
-	def _setup_npm(self) -> List[str]:
-		_NodeJSVersion = self.options.node_version
-		return [
-			"if [ ! -f ${SNAPCRAFT_PART_BUILD}/NodeJS/bin/node ]; then ",
-				"mkdir -p $SNAPCRAFT_PART_BUILD/NodeJS",
-				f"curl -s 'https://nodejs.org/dist/v{_NodeJSVersion}/node-v{_NodeJSVersion}-linux-x64.tar.xz' -o NodeJS.tar.xz",
-				"tar xf $SNAPCRAFT_PART_BUILD/NodeJS.tar.xz -C $SNAPCRAFT_PART_BUILD/NodeJS --strip-components 1",
-			"fi"
-		]
 
 	@staticmethod
 	def _apply_patches() -> List[str]:
@@ -56,10 +44,11 @@ class PluginImpl(PluginV2):
 			"npm cache verify",
 			"npm install",
 			"cd packages/app-desktop",
-			"npm run dist",
+			"node_modules/.bin/electron-rebuild",
+			"node_modules/.bin/electron-builder",
 			"cp -r dist/linux-unpacked ${SNAPCRAFT_PART_INSTALL}",
 			"mv ${SNAPCRAFT_PART_INSTALL}/linux-unpacked/@joplinapp-desktop ${SNAPCRAFT_PART_INSTALL}/linux-unpacked/joplin"
 		]
 
 	def get_build_commands(self) -> List[str]:
-		return self._apply_patches() + self._setup_npm() + self._build_commands()
+		return self._apply_patches() + self._build_commands()
